fce024558613aae660362c113a4df680c1f9588c
BuschMath <busch@iowacentral.edu>
2021-11-03 10:26:33 -0500

    Struct

--- Chapter10_Example_FA21/Main.cpp
+++ Chapter10_Example_FA21/Main.cpp
@@ -1,16 +1,54 @@
 #include <iostream>
-#include <climits>
+#include <string>
 
 using namespace std;
 
+typedef double SCALAR;
+
+enum Grade
+{
+	A,
+	B,
+	C,
+	D,
+	F
+};
+
+struct StudentRecord
+{
+	string firstName;
+	string lastName;
+	string className;
+	SCALAR gradePercent;
+	Grade gradeLetter;
+};
+
+struct ClassRecord
+{
+	StudentRecord student1;
+	StudentRecord student2;
+	StudentRecord student3;
+};
+
 int main()
 {
-	cout << "Size of:\nchar: " << sizeof(char) << "\n\nshort: " << sizeof(short) << "\n\nint: ";
-	cout << sizeof(int) << "\n\nlong: " << sizeof(long) << "\n\nbool: " << sizeof(bool) << "\n\n";
+	StudentRecord george;
+	george.firstName = "George";
+	george.lastName = "Smith";
+	george.className = "Calculus 1";
+	george.gradeLetter = B;
+	george.gradePercent = 0.87;
 
-	cout << "Min/Max:\nchar min/max: " << CHAR_MIN << " " << CHAR_MAX << "\n\nshort min/max: ";
-	cout << SHRT_MIN << " " << SHRT_MAX << "\n\nint min/max: " << INT_MIN << " " << INT_MAX;
-	cout << "\n\nlong min/max: " << LONG_MIN << " " << LONG_MAX << endl;
+	ClassRecord calculus;
+	calculus.student1 = george;
+
+	calculus.student2.firstName = "Adam";
+	calculus.student2.lastName = "Smith";
+	calculus.student2.className = "Calculus 1";
+	calculus.student2.gradeLetter = C;
+	calculus.student2.gradePercent = 0.71;
+
+	cout << "First Name: " << george.firstName << endl << endl;
 
 	return 0;
 }
