2cbeaa429d403a3bdf12761826f3cd4fbd4ac4e3
BuschMath <busch@iowacentral.edu>
2021-11-03 10:34:08 -0500

    Union

--- Chapter10_Example_FA21/Main.cpp
+++ Chapter10_Example_FA21/Main.cpp
@@ -1,54 +1,24 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 typedef double SCALAR;
 
-enum Grade
+union example
 {
-	A,
-	B,
-	C,
-	D,
-	F
-};
-
-struct StudentRecord
-{
-	string firstName;
-	string lastName;
-	string className;
-	SCALAR gradePercent;
-	Grade gradeLetter;
-};
-
-struct ClassRecord
-{
-	StudentRecord student1;
-	StudentRecord student2;
-	StudentRecord student3;
+	SCALAR s_point;
+	float f_point;
+	int i_point;
 };
 
 int main()
 {
-	StudentRecord george;
-	george.firstName = "George";
-	george.lastName = "Smith";
-	george.className = "Calculus 1";
-	george.gradeLetter = B;
-	george.gradePercent = 0.87;
+	example point;
 
-	ClassRecord calculus;
-	calculus.student1 = george;
+	point.f_point = 3.14;
+	point.i_point = 42;
 
-	calculus.student2.firstName = "Adam";
-	calculus.student2.lastName = "Smith";
-	calculus.student2.className = "Calculus 1";
-	calculus.student2.gradeLetter = C;
-	calculus.student2.gradePercent = 0.71;
-
-	cout << "First Name: " << george.firstName << endl << endl;
+	cout << point.f_point << " " << point.i_point << endl << endl;
 
 	return 0;
 }
